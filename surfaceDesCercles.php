3e807b15048e03b9543a624597923a60bbb97c82
Adrien <adrien.mechin@gmail.com>
2019-07-10 18:29:17 +0200

    Ex1 - Surfaces des Cercles

---
+++ surfaceDesCercles.php
@@ -0,0 +1,50 @@
+<?php
+
+?>
+
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<script>
+    /* Affichage du nom du script */
+    alert("1. Surface des cercles");
+
+    /* Déclaration de variables locales */
+    var rayon1, rayon2, pi, surface1, surface2, difference;
+
+    /* Initialisation des variables */
+    rayon1 = 5.5;
+    rayon2 = 3.5;
+    pi = 3.14;
+
+    /* Calculs */
+    surface1 = pi * rayon1 * rayon1;
+    surface2 = pi * rayon2 * rayon2;
+    difference = surface1 - surface2;
+
+    /* Affichage des résultats */
+    document.write("Surface 1 : " + surface1 + "<br />");
+    document.write("Surface 2 : " + surface2 + "<br />");
+    alert("Différence : " + difference);
+</script>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
